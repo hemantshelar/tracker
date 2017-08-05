0ea0c5306cf0577591429cc5032142ef5747ac92
Hemant Shelar <hemant.shelar@gmail.com>
2017-08-05 11:24:57 +1000

    Webpack up and running...

---
+++ readme.txt
@@ -0,0 +1,4 @@
+Use 'webpack --watch' from command line OR
+add 'watch: true' key to webpack.config.js file
+
+npm install css-loader style-loader 
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: './scripts/app.js',
+    output: {
+        filename: './scripts/bundle.js'
+    },
+    watch: true //This key will give effect of live-reload
+}
