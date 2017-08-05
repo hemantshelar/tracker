db8258bf07393b1a713a9e33d788e8c8076f8973
Hemant Shelar <hemant.shelar@gmail.com>
2017-08-05 12:23:44 +1000

    Configured bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         filename: './scripts/bundle.js'
     },
-    watch: true //This key will give effect of live-reload
+    watch: true, //This key will give effect of live-reload,
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader',
+            }
+        ]
+    }
 }
